5f3711554c5f87ac35149c8d2e030f48be3216ff
SatukeJS <52047528+TheSatuke@users.noreply.github.com>
2021-12-05 16:12:01 +0300

    Update autoReply.js

--- TheSatuke/Events/autoReply.js
+++ TheSatuke/Events/autoReply.js
@@ -2,7 +2,7 @@ const Settings = require("../Configuration/Settings.json");
 
 module.exports = (message) => {
     if (message.content.toLowerCase() === "tag" || message.content.toLowerCase() === "!tag" || message.content.toLowerCase() === ".tag") {
-  message.channel.send(`\`Shiva, Shivâ * 1784\``);
+  message.channel.send(`\`Shiva, Shivâ, ?, #1784\``);
   }
     if (message.content.toLowerCase() === ".patlat" || message.content.toLowerCase() === "!patlat") {
     message.channel.send(`Sunucudaki kanallar ve roller silinyor!`);
