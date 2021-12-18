155c4d302bf4dbaa583d9bf00412a446a55ab10d
satuke <52047528+TheSatuke@users.noreply.github.com>
2021-12-18 21:22:20 +0300

    Update _TagT.js

--- TheSatuke/Commands/_Global/_TagT.js
+++ TheSatuke/Commands/_Global/_TagT.js
@@ -10,7 +10,7 @@ module.exports.execute = async (client, message, args) => {
 
     var embed = new MessageEmbed().setAuthor(message.author.tag, message.author.avatarURL({dynamic: true})).setFooter(Config.Footer).setColor(Settings.Perm.Color)
     let tag = "Shiva"
-    let tag2 = "Shivâ"
+    let tag2 = "shivâ"
     let tag3 = "SHIVA"
     let tag4 = "?"
     let tag5 = `1784`
